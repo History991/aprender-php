8ce0f9606aaf10b27662486be8ac93c1c870dae6
Daniel R <info@saberahorrar.com>
2022-10-23 12:27:50 +0000

    prueba con operadores de mates

--- pruebas-libro/borrador-pruebas/operadores.php
+++ pruebas-libro/borrador-pruebas/operadores.php
@@ -1,3 +1,20 @@
 <?php
 echo 6 + 2; // mostramos el resultado de la suma de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 - 2; // mostramos el resultado de la resta de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 * 2; // mostramos el resultado de la multiplicación de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 / 2; // mostramos el resultado de la división de 6 y 2  
+echo "<br>"; // mostramos un salto de línea
+echo 6 % 2; // mostramos el resto de la división de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 ** 2; // mostramos el resultado de la potencia de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 + 2 * 3; // mostramos el resultado de la suma de 6 y 2 multiplicado por 3
+echo "<br>"; // mostramos un salto de línea
+echo (6 + 2) * 3; // mostramos el resultado de la suma de 6 y 2 multiplicado por 3
+echo "<br>"; // mostramos un salto de línea
+echo 6 + 2 * 3 ** 2; // mostramos el resultado de la suma de 6 y 2 multiplicado por 3 elevado a 2
+echo "<br>"; // mostramos un salto de línea
 ?>
