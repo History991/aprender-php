d83226b43def50eb7c106f6eca7c1cb175dbe75a
Daniel R <info@saberahorrar.com>
2022-10-23 12:17:45 +0000

    creacion comenntarios ejemplo 3-5

--- pruebas-libro/3-5_ejemplo.php
+++ pruebas-libro/3-5_ejemplo.php
@@ -1,7 +1,7 @@
 <?php
-$oxo = array(
-    array('x', ' ', 'o'),
-    array('o', 'o', 'x'),
-    array('x', 'o', ''));
-echo $oxo[1][2];
+$oxo = array(  // creamos un array con los valores "x", "o" y "x"
+    array('x', ' ', 'o'), // la primera fila del array
+    array('o', 'o', 'x'), // la segunda fila del array
+    array('x', 'o', '')); // la tercera fila del array
+echo $oxo[1][2]; // mostramos el valor de la segunda fila y tercera columna del array
 ?>
