c5615e11083daa09b3eb4d11f5b07fb62bcd5699
Daniel R <info@saberahorrar.com>
2022-10-23 12:16:08 +0000

    poner comentarios en ejemplo 3-4

--- pruebas-libro/3_4-test1.php
+++ pruebas-libro/3_4-test1.php
@@ -1,7 +1,7 @@
 <?php
-$username = "daniel prueba";
-echo $username;
-echo "<br>";
-$current_user = $username;
-echo $current_user;
+$username = "daniel prueba"; // asignamos el valor "daniel prueba" a la variable $username
+echo $username; // mostramos el valor de la variable $username
+echo "<br>"; // mostramos un salto de línea
+$current_user = $username; // asignamos el valor de la variable $username a la variable $current_user
+echo $current_user; // mostramos el valor de la variable $current_user
 ?>
