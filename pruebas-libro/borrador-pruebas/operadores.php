50930d8e1689dea2dd869205e8e95a61f3c4bf96
Daniel R <info@saberahorrar.com>
2022-10-23 12:35:28 +0000

    añadido mas ejemplos de operadores

--- pruebas-libro/borrador-pruebas/operadores.php
+++ pruebas-libro/borrador-pruebas/operadores.php
@@ -17,4 +17,44 @@ echo (6 + 2) * 3; // mostramos el resultado de la suma de 6 y 2 multiplicado por
 echo "<br>"; // mostramos un salto de línea
 echo 6 + 2 * 3 ** 2; // mostramos el resultado de la suma de 6 y 2 multiplicado por 3 elevado a 2
 echo "<br>"; // mostramos un salto de línea
+// a partir de aqui son ejemplos de operadores de asignación
+$a = 6; // asignamos el valor 6 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+$a += 2; // sumamos 2 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+$a -= 2; // restamos 2 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+$a *= 2; // multiplicamos 2 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+$a /= 2; // dividimos 2 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+$a %= 2; // obtenemos el resto de la división de 2 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+$a **= 2; // elevamos 2 a la variable $a
+echo $a; // mostramos el valor de la variable $a
+echo "<br>"; // mostramos un salto de línea
+// a partir de aqui son ejemplos de operadores de comparación
+echo 6 == 2; // mostramos el resultado de la comparación de igualdad de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 != 2; // mostramos el resultado de la comparación de desigualdad de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 > 2; // mostramos el resultado de la comparación de mayor que de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 < 2; // mostramos el resultado de la comparación de menor que de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 >= 2; // mostramos el resultado de la comparación de mayor o igual que de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 <= 2; // mostramos el resultado de la comparación de menor o igual que de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 === 2; // mostramos el resultado de la comparación de identidad de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+echo 6 !== 2; // mostramos el resultado de la comparación de no identidad de 6 y 2
+echo "<br>"; // mostramos un salto de línea
+// a partir de aqui son ejemplos de operadores lógicos
 ?>
